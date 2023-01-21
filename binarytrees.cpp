ef5ec7773ece02f90f18624058161ae7a78770ef
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-01-21 10:37:15 +0530

    added binary trees implementation code

--- binarytrees.cpp
+++ binarytrees.cpp
@@ -65,15 +65,91 @@ void levelOrderTraversal(Node* root){
                 q.push(temp->right);
             }
         }
-        
+    }
+}
+
+//L N R
+void inOrderTraversal(Node* root){
+    if(root == NULL){
+        return;
+    }
+    inOrderTraversal(root->left);
+    cout<<root->data<<" ";
+    inOrderTraversal(root->right);
+}
+
+//N L R 
+void preOrderTraversal(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    cout<<root->data<<" ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+
+//L R N
+void postOrderTraversal(Node* root){
+    if(root == NULL){
+        return;
+    }
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout<<root->data<<" ";
+}
+
+void buildFromLvlOrder(Node* &root){
+    queue<Node*> q;
+    int data;
+    cout<<"Enter data"<<endl;
+    cin>>data;
+    root = new Node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        //Taking left node
+        cout<<"Enter left Node for: "<<temp->data<<endl;
+        int ldata;
+        cin>>ldata;
+        if(ldata != -1){
+            temp->left = new Node(ldata);
+            q.push(temp->left);
+        }
+
+        //Taking right node
+        cout<<"Enter right Node for: "<<temp->data<<endl;
+        int rdata;
+        cin>>rdata;
+        if(rdata != -1){
+            temp->right = new Node(rdata);
+            q.push(temp->right);
+        }
     }
 }
 
 int main(){
     Node* root = NULL;
-    root = buildTree(root);
-    //1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    // root = buildTree(root);
+    
+    buildFromLvlOrder(root);
+
+
+    //1 3 5 7 11 17 -1 -1 -1 -1 -1 -1 -1
+
     cout<<"Level order traversal: "<<endl;
     levelOrderTraversal(root);
+
+    cout<<"Inorder Traversal: "<<endl;
+    inOrderTraversal(root);
+    cout<<endl;
+    cout<<"Postorder Traversal: "<<endl;
+    postOrderTraversal(root);
+    cout<<endl;
+    cout<<"Preorder Traversal: "<<endl;
+    preOrderTraversal(root);
     return 0;
 }
